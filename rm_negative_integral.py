9ae136d59ad2ef45ae8ae001522ebe1bd09ea0bf
Towsifa Akhter <51368122+akhter-towsifa@users.noreply.github.com>
2025-10-07 09:05:53 -0500

    Simplify check for non-empty process_to_remove

--- rm_negative_integral.py
+++ rm_negative_integral.py
@@ -16,7 +16,7 @@ for channel in channels:
                     # print(process_to_remove)
             else:
                 continue
-        if process_to_remove is not []:
+        if process_to_remove:
             print(f"file: hh_{category}_{channel}_2022_13p6TeV.input.root")
             print(f"Process to remove: {process_to_remove}")    
             # python3 remove_processes.py /eos/user/t/toakhter/bin_opt_tests/bin_opt_test_oct_hamburg_v2/hh_{category}_{channel}_2022_13p6TeV.txt $process_to_remove -d /eos/user/t/toakhter/bin_opt_tests/bin_opt_test_oct_hamburg_v2/
